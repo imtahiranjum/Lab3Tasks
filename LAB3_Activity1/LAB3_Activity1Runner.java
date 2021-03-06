1c449037f58b5337975c887987a31a64f00fc91e
Tahir Anjum <78358354+imtahiranjum@users.noreply.github.com>
2021-03-07 01:22:25 +0500

    Add files via upload

---
+++ LAB3_Activity1/LAB3_Activity1Runner.java
@@ -0,0 +1,8 @@
+public class LAB3_Activity1Runner {
+    public static void main(String[] args){
+        RectangleActivity firstRectangle = new RectangleActivity();
+        firstRectangle.setLength();
+        firstRectangle.setWidth();
+        firstRectangle.display();
+    }
+}
