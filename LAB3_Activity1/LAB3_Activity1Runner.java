e864963c6f45bc329f713fa5cad3d47d06460360
Tahir Anjum <78358354+imtahiranjum@users.noreply.github.com>
2021-03-08 02:44:21 +0500

    Delete LAB3_Activity1 directory

--- LAB3_Activity1/LAB3_Activity1Runner.java
+++
@@ -1,8 +0,0 @@
-public class LAB3_Activity1Runner {
-    public static void main(String[] args){
-        RectangleActivity firstRectangle = new RectangleActivity();
-        firstRectangle.setLength();
-        firstRectangle.setWidth();
-        firstRectangle.display();
-    }
-}
