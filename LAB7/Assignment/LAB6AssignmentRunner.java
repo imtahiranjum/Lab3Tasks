ec041295d2e82e3fa6cb700c0e6a1986692d4fba
Tahir Anjum <78358354+imtahiranjum@users.noreply.github.com>
2021-04-13 00:18:57 +0500

    Update LAB6AssignmentRunner.java

--- LAB7/Assignment/LAB6AssignmentRunner.java
+++ LAB7/Assignment/LAB6AssignmentRunner.java
@@ -6,10 +6,15 @@ public class LAB6AssignmentRunner {
         Pizza first = new Pizza("Medium", 2, 3,1);
         Pizza second = new Pizza("Medium", 5, 1,1);
 
+        PizzaOrder firstOrder = new PizzaOrder(first, second);
+
         first.calcCost();
         second.calcCost();
 
         first.getDescription();
         second.getDescription();
+
+        firstOrder.calcTotal();
+
     }
 }
