6b2ba7c865482449eef20b25e02a2b1a47f2027c
Tahir Anjum <78358354+imtahiranjum@users.noreply.github.com>
2021-04-20 01:44:53 +0500

    Update LAB8_Q2_Runner.java

--- LAB8/Assignment/Question2/LAB8_Q2_Runner.java
+++ LAB8/Assignment/Question2/LAB8_Q2_Runner.java
@@ -1,8 +1,10 @@
 package Question2;
 
 public class LAB8_Q2_Runner {
-    Computer first = new Computer(5,5,9,7);
-    Laptop second = new Laptop();
-    first.display();
-    second.display();
+    public static void main(String[] args){
+        Computer first = new Computer(5,5,9,7);
+        Laptop second = new Laptop();
+        first.display();
+        second.display();
+    }
 }
